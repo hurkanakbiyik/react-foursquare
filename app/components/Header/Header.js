a6272c26ee8c08b83ea6322ba2dab4d2ee1fba71
hurkanakbiyik <hurkanakbiyik@gmail.com>
2018-05-29 14:43:54 +0300

    Header changed for foursquare. Banner added. Search bar added.

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import MdSearch from 'react-icons/lib/md/search';
+
 import './style.scss';
+import banner from './images/banner.png';
 
 class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <div className="header">
+        <img alt="banner" src={banner} />
+        <div className="search-area">
+          <MdSearch size={25} />
+          <input type="text" placeholder={'Search'} />
+        </div>
       </div>
     );
   }
